fac84f39f8b669a85ac3c6937d9806a825bcd67d
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-12-07 18:17:47 -0700

    Added routes for all resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api do # /api/data
-
-    get '/data', to: 'tests#index'
-    
-    resources :dogs
-
+  namespace :api do
+    resources :generations do
+      resources :pokemons do
+        resources :forms
+      end
+    end
+    resources :fast_moves do
+      resources :pokemon_fast_moves
+    end
+    resources :charge_moves do
+      resources :pokemon_charge_moves
+    end
+    resources :types do
+      resources :type_interactions
+    end
+    resources :families
+    resources :pokemon_levels
+    resources :medals
   end
 
   get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
