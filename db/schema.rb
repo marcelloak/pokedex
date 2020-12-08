64644156c4d1ec63d199df8d84437ad2c402f263
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-12-07 18:17:27 -0700

    Changed db name and fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,50 +15,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_215439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "badges", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "tier"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "board_tiles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "board_id"
-    t.bigint "tile_id"
-    t.index ["board_id"], name: "index_board_tiles_on_board_id"
-    t.index ["tile_id"], name: "index_board_tiles_on_tile_id"
-  end
-
-  create_table "boards", force: :cascade do |t|
-    t.datetime "ended_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "game_id"
-    t.index ["game_id"], name: "index_boards_on_game_id"
-  end
-
-  create_table "books", force: :cascade do |t|
-    t.string "name"
-    t.string "author"
-    t.text "cover_image"
-    t.string "genre"
-    t.string "isbn"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "cards", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "effect"
-    t.integer "outcome"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "charge_moves", force: :cascade do |t|
     t.string "name"
     t.integer "damage"
@@ -71,13 +27,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_215439) do
     t.datetime "updated_at", null: false
     t.bigint "type_id"
     t.index ["type_id"], name: "index_charge_moves_on_type_id"
-  end
-
-  create_table "colors", force: :cascade do |t|
-    t.string "name"
-    t.string "hexcode"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "families", force: :cascade do |t|
@@ -117,24 +66,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_215439) do
     t.index ["secondary_type_id"], name: "index_forms_on_secondary_type_id"
   end
 
-  create_table "games", force: :cascade do |t|
-    t.string "win_requirement"
-    t.integer "win_points"
-    t.boolean "isbn_trust"
-    t.boolean "isbn_master"
-    t.boolean "isbn_vote"
-    t.integer "turn_delay"
-    t.integer "turn_reminder"
-    t.string "name"
-    t.string "password_digest"
-    t.integer "max_players"
-    t.datetime "ended_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_games_on_user_id"
-  end
-
   create_table "generations", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -150,59 +81,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_215439) do
     t.string "reward"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "player_badges", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "player_id"
-    t.bigint "board_id"
-    t.bigint "badge_id"
-    t.index ["badge_id"], name: "index_player_badges_on_badge_id"
-    t.index ["board_id"], name: "index_player_badges_on_board_id"
-    t.index ["player_id"], name: "index_player_badges_on_player_id"
-  end
-
-  create_table "player_cards", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "player_id"
-    t.bigint "board_id"
-    t.bigint "card_id"
-    t.index ["board_id"], name: "index_player_cards_on_board_id"
-    t.index ["card_id"], name: "index_player_cards_on_card_id"
-    t.index ["player_id"], name: "index_player_cards_on_player_id"
-  end
-
-  create_table "player_tiles", force: :cascade do |t|
-    t.datetime "ended_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "player_id"
-    t.bigint "board_tile_id"
-    t.bigint "book_id"
-    t.bigint "review_id"
-    t.index ["board_tile_id"], name: "index_player_tiles_on_board_tile_id"
-    t.index ["book_id"], name: "index_player_tiles_on_book_id"
-    t.index ["player_id"], name: "index_player_tiles_on_player_id"
-    t.index ["review_id"], name: "index_player_tiles_on_review_id"
-  end
-
-  create_table "players", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "game_id"
-    t.bigint "color_id"
-    t.bigint "token_id"
-    t.integer "position"
-    t.integer "score"
-    t.boolean "moving"
-    t.integer "final_position"
-    t.index ["color_id"], name: "index_players_on_color_id"
-    t.index ["game_id"], name: "index_players_on_game_id"
-    t.index ["token_id"], name: "index_players_on_token_id"
-    t.index ["user_id"], name: "index_players_on_user_id"
   end
 
   create_table "pokemon_charge_moves", force: :cascade do |t|
@@ -266,62 +144,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_215439) do
     t.index ["secondary_type_id"], name: "index_pokemons_on_secondary_type_id"
   end
 
-  create_table "recommendations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "book_id"
-    t.bigint "tile_id"
-    t.index ["book_id"], name: "index_recommendations_on_book_id"
-    t.index ["tile_id"], name: "index_recommendations_on_tile_id"
-  end
-
-  create_table "results", force: :cascade do |t|
-    t.integer "score"
-    t.integer "books"
-    t.boolean "winner"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "player_id"
-    t.bigint "board_id"
-    t.index ["board_id"], name: "index_results_on_board_id"
-    t.index ["player_id"], name: "index_results_on_player_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.text "review_text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "book_id"
-    t.bigint "user_id"
-    t.index ["book_id"], name: "index_reviews_on_book_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
-  create_table "tile_groups", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "color_id"
-    t.index ["color_id"], name: "index_tile_groups_on_color_id"
-  end
-
-  create_table "tiles", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "tile_group_id"
-    t.index ["tile_group_id"], name: "index_tiles_on_tile_group_id"
-  end
-
-  create_table "tokens", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "type_interactions", force: :cascade do |t|
     t.float "multiplier"
     t.datetime "created_at", null: false
@@ -339,47 +161,13 @@ ActiveRecord::Schema.define(version: 2020_12_07_215439) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "board_tiles", "boards"
-  add_foreign_key "board_tiles", "tiles"
-  add_foreign_key "boards", "games"
   add_foreign_key "charge_moves", "types"
   add_foreign_key "fast_moves", "types"
   add_foreign_key "forms", "pokemons"
-  add_foreign_key "games", "users"
-  add_foreign_key "player_badges", "badges"
-  add_foreign_key "player_badges", "boards"
-  add_foreign_key "player_badges", "players"
-  add_foreign_key "player_cards", "boards"
-  add_foreign_key "player_cards", "cards"
-  add_foreign_key "player_cards", "players"
-  add_foreign_key "player_tiles", "board_tiles"
-  add_foreign_key "player_tiles", "books"
-  add_foreign_key "player_tiles", "players"
-  add_foreign_key "player_tiles", "reviews"
-  add_foreign_key "players", "colors"
-  add_foreign_key "players", "games"
-  add_foreign_key "players", "tokens"
-  add_foreign_key "players", "users"
   add_foreign_key "pokemon_charge_moves", "charge_moves"
   add_foreign_key "pokemon_charge_moves", "pokemons"
   add_foreign_key "pokemon_fast_moves", "fast_moves"
   add_foreign_key "pokemon_fast_moves", "pokemons"
   add_foreign_key "pokemons", "families"
   add_foreign_key "pokemons", "generations"
-  add_foreign_key "recommendations", "books"
-  add_foreign_key "recommendations", "tiles"
-  add_foreign_key "results", "boards"
-  add_foreign_key "results", "players"
-  add_foreign_key "reviews", "books"
-  add_foreign_key "reviews", "users"
-  add_foreign_key "tile_groups", "colors"
-  add_foreign_key "tiles", "tile_groups"
 end
