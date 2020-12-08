9f6c658eb81160225f486c0a13b7b41aa9f80953
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-12-07 18:18:01 -0700

    Added generations controller

---
+++ app/controllers/api/generations_controller.rb
@@ -0,0 +1,51 @@
+class Api::GenerationsController < ApplicationController
+  def index
+    generations = Generation.all
+
+    render :json => generations
+  end
+
+  def show
+    generation = Generation.find(params[:id])
+
+    render :json => generation
+  end
+
+  def create
+    generation = Generation.new(generation_params)
+
+    if generation.save
+      render :json => generation
+    else
+      render :json => {
+        error: 'Generation was not saved'
+      }
+    end
+  end
+
+  def update
+    generation = Generation.find(params[:id])
+
+    if generation.update(generation_params)
+      render :json => generation
+    else
+      render :json => {
+        error: 'Generation was not updated'
+      }
+    end
+  end
+
+  def destroy
+    generation = Generation.find(params[:id])
+    generation.destroy
+
+    render :json => {
+      message: 'Generation was destroyed'
+    }
+  end
+
+  private
+    def generation_params
+      params.permit(:name)
+    end
+end
--- client/src/components/forms/Generation.js
+++ client/src/components/forms/Generation.js
@@ -1,26 +1,42 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, Fragment } from "react"
 import axios from 'axios'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 
 export default function Generation() {
-  const [name, setName] = useState("")
+  const [name, setName] = useState('')
+  const [generations, setGenerations] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/generations')
+    .then((response) => {
+      setGenerations(response.data)
+    })
+  }, []);
 
   const clickHandler = () => {
     if (name) {
       axios.post('/api/generations', { name: name })
+      setGenerations((current) => [...current, { name }])
     }
   }
 
   const submitHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+    clickHandler()
+    setName('')
   }
 
   return (
-    <form onSubmit={submitHandler} className="generation-form">
-      <TextField id="standard-basic" label="Generation" type="text"  name="name" value={name} onChange={(event) => setName(event.target.value)}/>
-      <br /> 
-      <Button variant="outlined" style={{ fontSize: '1em', fontWeight: 'bolder' }} onClick={clickHandler}>Submit</Button>
-    </form>
+    <Fragment>
+      {generations.map((generation, index) => {
+        return <p key={index}>{generation.name}</p>
+      })}
+      <form onSubmit={submitHandler} className="generation-form">
+        <TextField id="standard-basic" label="Generation" type="text"  name="name" value={name} onChange={(event) => setName(event.target.value)}/>
+        <br /> 
+        <Button variant="outlined" style={{ fontSize: '1em', fontWeight: 'bolder' }} onClick={clickHandler}>Submit</Button>
+      </form>
+    </Fragment>
   )
 }
