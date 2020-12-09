13be438a9358500316e22971641e8b0d750c84af
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-12-08 17:38:53 -0700

    Altered react routes and links to be variable based on table api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home'
 import Nav from './components/Nav'
-import Generation from './components/forms/Generation'
-import Pokemon from './components/forms/Pokemon'
+import Form from './components/Form'
 
 export default function App() {
+  const [tables, setTables] = useState([])
+
+  useEffect(() => {
+    axios.get('/tables')
+    .then((response) => {
+      setTables(response.data)
+    })
+  }, []);
+
+  const tableRoutes = function() {
+    return tables.map((table, index) => {
+      return <Route key={index} path={`/${table.name}`} render={(props) => (<Form {...props} table={table} />)} />
+    })
+  }
+
   return (
     <Router>
       <Nav />
       <Switch>
-        <Route exact path="/" render={(props) => (<Home {...props} />)} />
-        <Route path="/generations" render={(props) => (<Generation {...props} />)} />
-        <Route path="/pokemon" render={(props) => (<Pokemon {...props} />)} />
+        <Route exact path="/" render={(props) => (<Home {...props} tables={tables} />)} />
+        {tableRoutes()}
       </Switch>
     </Router>
   )
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { Link } from "react-router-dom";
 
-export default function Home() {
+export default function Home(props) {
+  const titleCase = function(str) {
+    const splitStr = str.toLowerCase().split(/[\s_]+/);
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+  }
+
+  const tableLinks = function() {
+    return props.tables.map((table, index) => {
+      return (
+        <Fragment key={index}>
+          <Link to={`/${table.name}`} >{titleCase(table.name)}</Link>
+          <br/>
+        </Fragment>
+      )
+    })
+  }
+
   return (
     <div className="home-page">
-      <Link to={'/generations'} >Generations</Link>
-      <br/>
-      <Link to={'/pokemon'} >Pokemon</Link>
+      {tableLinks()}
     </div>
   )
 }
