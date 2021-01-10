c03c3c9a0678718be1aa721e91384f3787364486
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2021-01-10 14:28:09 -0700

    Added getting, displaying and sorting records in pokedex routes

--- client/src/components/pokedex/PokedexRoute.js
+++ client/src/components/pokedex/PokedexRoute.js
@@ -1,6 +1,19 @@
-import React from "react"
+import React, { useState, useEffect, Fragment } from "react"
+import axios from 'axios'
 
 export default function PokedexRoute(props) {
+  const [columns, setColumns] = useState([])
+  const [records, setRecords] = useState([])
+  const [sort, setSort] = useState({ key: 'id', asc: true})
+
+  useEffect(() => {
+    axios.get(`/${props.route}`)
+    .then((response) => {
+      setColumns(response.data[0])
+      setRecords(response.data.slice(1))
+    })
+  }, []);
+
   const titleCase = function(str) {
     const splitStr = str.toLowerCase().split(/[\s_]+/);
     for (var i = 0; i < splitStr.length; i++) {
@@ -9,7 +22,73 @@ export default function PokedexRoute(props) {
     return splitStr.filter(str => str !== 'Id').join(' '); 
   }
 
+  const changeSort = function(column) {
+    setSort((current) => {
+      return { key: column, asc: current.key === column ? !current.asc : true }
+    })
+  }
+
+  const compare = function(sort) {
+    const c = function(a, b) {
+      if (a[sort.key] && b[sort.key]) {
+        if (a[sort.key].name || b[sort.key].name) {
+          if (a[sort.key].name && b[sort.key].name) {
+            if (a[sort.key].name < b[sort.key].name) return sort.asc ? -1 : 1
+            if (a[sort.key].name > b[sort.key].name) return sort.asc ? 1 : -1
+            return 0
+          }
+          else if (a[sort.key].name) return sort.asc ? -1 : 1
+          else if (b[sort.key].name) return sort.asc ? 1 : -1
+          else return 0
+        }
+        else {
+          if (a[sort.key] < b[sort.key]) return sort.asc ? -1 : 1
+          if (a[sort.key] > b[sort.key]) return sort.asc ? 1 : -1
+          return 0
+        }
+      }
+      else if (a[sort.key]) return sort.asc ? -1 : 1
+      else if (b[sort.key]) return sort.asc ? 1 : -1
+      else return 0
+    }
+
+    return c
+  }
+
+  const getRecordRows = function() {
+    return (<table>
+      <thead>
+        <tr>
+          {columns.filter((column) => column !== 'created_at' && column !== 'updated_at').map((column, index) => {
+            return <th key={index} onClick={() => changeSort(column)} >{titleCase(column)}</th>
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {records.sort(compare(sort)).map((record, index) => {
+          return (
+            <tr key={index}>
+              {columns.filter((column) => column !== 'created_at' && column !== 'updated_at').map((column, index) => {
+                if (String(record[column]).includes('.png')) return <td key={index}><img src={record[column]} alt={record[column]} width={30} height={30}/></td>
+                else if (String(column).includes('_id'))  {
+                  if (String(record[column].name).includes('.png')) return <td key={index}><img src={record[column].name} alt={record[column].name} width={30} height={30}/></td>
+                  else return <td key={index}>{record[column].name}</td>
+                }
+                else if (String(record[column]).includes('T00:00:00.000Z')) return <td key={index}>{record[column].split('T')[0]}</td>
+                else if (record[column] === true) return <td key={index}>Y</td>
+                else return <td key={index}>{record[column]}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>)
+  }
+
   return (
-    <h3>{titleCase(props.route)}</h3>
+    <Fragment>
+      <h3>{titleCase(props.route)}</h3>
+      {getRecordRows()}
+    </Fragment>
   )
 }
