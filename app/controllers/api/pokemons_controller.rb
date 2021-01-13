61b3dd1735d34e2eebf7a0dea4cadf48dccdfb9f
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2021-01-13 13:18:03 -0700

    Made getting all pokemon faster

--- app/controllers/api/pokemons_controller.rb
+++ app/controllers/api/pokemons_controller.rb
@@ -1,14 +1,17 @@
 class Api::PokemonsController < ApplicationController
   def index
     pokemons = Pokemon.all.order(:id)
+    generations = Generation.all
+    families = Family.all
+    types = Type.all
     pokemons = pokemons.as_json.map { |pokemon|
       {
         **pokemon.symbolize_keys,
-        evolves_from_id: { id: pokemon.symbolize_keys[:evolves_from_id], name: pokemon.symbolize_keys[:evolves_from_id] ? Pokemon.find(pokemon.symbolize_keys[:evolves_from_id])[:name] : pokemon.symbolize_keys[:evolves_from_id] },
-        generation_id: { id: pokemon.symbolize_keys[:generation_id], name: Generation.find(pokemon.symbolize_keys[:generation_id])[:name] },
-        family_id: { id: pokemon.symbolize_keys[:family_id], name: Family.find(pokemon.symbolize_keys[:family_id])[:name] },
-        primary_type_id: { id: pokemon.symbolize_keys[:primary_type_id], name: Type.find(pokemon.symbolize_keys[:primary_type_id])[:icon] },
-        secondary_type_id: { id: pokemon.symbolize_keys[:secondary_type_id], name: pokemon.symbolize_keys[:secondary_type_id] ? Type.find(pokemon.symbolize_keys[:secondary_type_id])[:icon] : pokemon.symbolize_keys[:secondary_type_id] }
+        evolves_from_id: { id: pokemon.symbolize_keys[:evolves_from_id], name: pokemon.symbolize_keys[:evolves_from_id] ? (pokemons.find {|x| x[:id] == pokemon.symbolize_keys[:evolves_from_id]})[:name] : pokemon.symbolize_keys[:evolves_from_id] },
+        generation_id: { id: pokemon.symbolize_keys[:generation_id], name: (generations.find {|x| x[:id] == pokemon.symbolize_keys[:generation_id]})[:name] },
+        family_id: { id: pokemon.symbolize_keys[:family_id], name: (families.find {|x| x[:id] == pokemon.symbolize_keys[:family_id]})[:name] },
+        primary_type_id: { id: pokemon.symbolize_keys[:primary_type_id], name: (types.find {|x| x[:id] == pokemon.symbolize_keys[:primary_type_id]})[:icon] },
+        secondary_type_id: { id: pokemon.symbolize_keys[:secondary_type_id], name: pokemon.symbolize_keys[:secondary_type_id] ? (types.find {|x| x[:id] == pokemon.symbolize_keys[:secondary_type_id]})[:icon] : pokemon.symbolize_keys[:secondary_type_id] }
       }
     }
 
--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -1,20 +1,21 @@
-require 'date'
-
 class PokedexController < ApplicationController
   def released_pokemon
+    pokemons = Pokemon.all
+    generations = Generation.all
+    families = Family.all
+    types = Type.all
     released = PokemonTimeline.all.pluck(:pokemon_id)
-    pokemons = Pokemon.all.select { |pokemon| released.include?(pokemon[:id])}
-    pokemons = pokemons.as_json.map { |pokemon|
+    released = (pokemons.select { |pokemon| released.include?(pokemon[:id])}).as_json.map { |pokemon|
       {
         **pokemon.symbolize_keys,
-        evolves_from_id: { id: pokemon.symbolize_keys[:evolves_from_id], name: pokemon.symbolize_keys[:evolves_from_id] ? Pokemon.find(pokemon.symbolize_keys[:evolves_from_id])[:name] : pokemon.symbolize_keys[:evolves_from_id] },
-        generation_id: { id: pokemon.symbolize_keys[:generation_id], name: Generation.find(pokemon.symbolize_keys[:generation_id])[:name] },
-        family_id: { id: pokemon.symbolize_keys[:family_id], name: Family.find(pokemon.symbolize_keys[:family_id])[:name] },
-        primary_type_id: { id: pokemon.symbolize_keys[:primary_type_id], name: Type.find(pokemon.symbolize_keys[:primary_type_id])[:icon] },
-        secondary_type_id: { id: pokemon.symbolize_keys[:secondary_type_id], name: pokemon.symbolize_keys[:secondary_type_id] ? Type.find(pokemon.symbolize_keys[:secondary_type_id])[:icon] : pokemon.symbolize_keys[:secondary_type_id] }
+        evolves_from_id: { id: pokemon.symbolize_keys[:evolves_from_id], name: pokemon.symbolize_keys[:evolves_from_id] ? (pokemons.find {|x| x[:id] == pokemon.symbolize_keys[:evolves_from_id]})[:name] : pokemon.symbolize_keys[:evolves_from_id] },
+        generation_id: { id: pokemon.symbolize_keys[:generation_id], name: (generations.find {|x| x[:id] == pokemon.symbolize_keys[:generation_id]})[:name] },
+        family_id: { id: pokemon.symbolize_keys[:family_id], name: (families.find {|x| x[:id] == pokemon.symbolize_keys[:family_id]})[:name] },
+        primary_type_id: { id: pokemon.symbolize_keys[:primary_type_id], name: (types.find {|x| x[:id] == pokemon.symbolize_keys[:primary_type_id]})[:icon] },
+        secondary_type_id: { id: pokemon.symbolize_keys[:secondary_type_id], name: pokemon.symbolize_keys[:secondary_type_id] ? (types.find {|x| x[:id] == pokemon.symbolize_keys[:secondary_type_id]})[:icon] : pokemon.symbolize_keys[:secondary_type_id] }
       }
     }
-    render :json => conversion(pokemons)
+    render :json => conversion(released)
   end
   
   def released_shinies
