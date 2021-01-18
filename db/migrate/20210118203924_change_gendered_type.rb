930053b33ab031c76c45425c39132c306eeb5a75
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2021-01-18 13:52:29 -0700

    Fixed column type change

--- db/migrate/20210118203924_change_gendered_type.rb
+++ db/migrate/20210118203924_change_gendered_type.rb
@@ -1,5 +1,11 @@
 class ChangeGenderedType < ActiveRecord::Migration[5.2]
   def change
-    change_column :pokemons, :gendered, :integer
+    def up
+      change_column :pokemons, :gendered, 'integer USING CAST(gendered AS integer)'
+    end
+
+    def down
+
+    end
   end
 end
